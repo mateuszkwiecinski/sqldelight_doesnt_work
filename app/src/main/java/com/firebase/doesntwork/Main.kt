1a509e3e54a68b3b2bd6f0696ce82cf5de557633
Mateusz Kwieciński <mateusz.kwiatek@gmail.com>
2021-11-24 09:02:08 +0100

    wip

--- app/src/main/java/com/firebase/doesntwork/Main.kt
+++ app/src/main/java/com/firebase/doesntwork/Main.kt
@@ -8,5 +8,5 @@ import com.squareup.sqldelight.db.SqlDriver
 fun main(context: Context) {
     val driver: SqlDriver = AndroidSqliteDriver(Database.Schema, context)
     val db = Database(driver = driver)
-    db.preferencesQueries.insert(Preference(key = "key", value_ = "value"))
+    db.preferencesQueries.insert(Preference(key = "key", value = "value"))
 }
